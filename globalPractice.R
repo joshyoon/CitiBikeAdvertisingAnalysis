ecb469f7200edad5c2fd41143f4bdfe9077c8f3e
joshyoon <jyoon716@gmail.com>
2017-10-10 12:35:55 -0400

    Delete globalPractice.R

--- globalPractice.R
+++
@@ -1,9 +0,0 @@
-state_stat <- data.frame(state.name = rownames(state.x77), state.x77)
-state.x77
-# remove row names
-rownames(state_stat) <- NULL
-# create variable with colnames as choice 
-choice <- colnames(state_stat)[-1]
-
-devtools::install_github("rstudio/DT")
-library(DT)
